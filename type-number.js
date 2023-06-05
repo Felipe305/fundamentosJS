4ae1f0f5aaecad9ae22ae5429de089f82f792bec
Felipe Silva <felipecs@motorola.com>
2023-06-05 11:29:24 -0300

    em andamento

---
+++ type-number.js
@@ -0,0 +1,32 @@
+// tipo Number
+
+const meuNumero = 3;
+
+const primeiroNumero = 1;
+const segundoNumero = 2;
+
+const operacaoMatematica = primeiroNumero - segundoNumero;
+
+console.log(operacaoMatematica);
+
+// ponto flutuante
+
+const numeroPontoflutuante = 3.3;
+const pontoFlutuanteSemZero = .5;
+
+const novaOperacao = primeiroNumero / numeroPontoflutuante; 
+
+console.log(novaOperacao);
+
+// NaN -> Not a Number (não é um número)
+
+const alura = "Alura";
+console.log(alura * primeiroNumero);
+
+var a = 10
+var b = 0
+console.log(a/b) // Infinity
+
+var a = 0
+var b = 0
+console.log(a/b) // NaN
---
+++ type-string.js
@@ -0,0 +1,15 @@
+const texto1 = "Olá, mundo!";
+const texto2 = 'Olá, mundo!';
+const senha = "senhaSuperSegura456!";
+const StringNumeros = "34567"
+
+const citacao = "meu nome é "
+const meuNome = "Felipe"
+
+// concatenação (+)
+
+console.log(citacao + meuNome)
+
+// template string OU template literal
+
+
